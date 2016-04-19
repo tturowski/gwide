ebb4edc0a2f46a044cf0e3455fd63a4642d6a88a
Tomasz W. Turowski <twturowski@gmail.com>
2016-04-19 13:10:36 +0100

    gwiderRNA - bug fixed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gwide',
-    version='0.3.16',
+    version='0.3.17',
     # py_modules=['gwide'],
     packages=find_packages(),
     install_requires=[
