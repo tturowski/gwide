f0f145b4e3b6d1318bc3bfafee26ce249569ea53
Tomasz W. Turowski <twturowski@gmail.com>
2016-04-22 10:27:56 +0100

    gwidePlot --LRR function added

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gwide',
-    version='0.3.18',
+    version='0.3.19',
     # py_modules=['gwide'],
     packages=find_packages(),
     install_requires=[
