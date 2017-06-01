87d221c855bfa424f73f018f2d22aac2bc1eaf26
tturowski <twturowski@gmail.com>
2017-06-01 16:59:32 +0100

    --antisense reads

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gwide',
-    version='0.5.9',
+    version='0.5.10',
     # py_modules=['gwide'],
     packages=find_packages(),
     install_requires=[
