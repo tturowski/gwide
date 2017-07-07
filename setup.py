822cc06459eab0657d95371489b90189dcd3cecc
tturowski <twturowski@gmail.com>
2017-07-07 20:31:58 +0100

    novo2concat --CDS added

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gwide',
-    version='0.5.12',
+    version='0.5.13',
     # py_modules=['gwide'],
     packages=find_packages(),
     install_requires=[
